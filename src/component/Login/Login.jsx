6b3765accd918e888bffee973eb7d1a116b7c2c6
Zaid_ZiD <zid6177@gmail.com>
2022-10-12 14:34:59 +0500

    changes in input field

--- src/component/Login/Login.jsx
+++ src/component/Login/Login.jsx
@@ -3,35 +3,49 @@ import "./Login.css"
 export default function Login() {
   return (
     <div className="login">
-        <div className="container1"></div>
-        <div className="container2">
-         <form>
+      <div className="container1"></div>
+      <div className="container2">
+        <form>
           <h1 className="heading">Login to continue</h1>
           <div className="inputBox">
-           <input className="inputs" type="email" placeholder="Email"/>
-           <input className="inputs" type="password" placeholder="Password" />
-          
 
-          <div className="forgetDiv">
-          <div className="checkbox">
-           <input type="checkbox" className="check"/>
-           <span>Remember Me</span>
+
+            <div className="inputSubBox">
+
+              <div className="input-container">
+                <input id="name" className="input" type="email" pattern="" required />
+                <label className="label" htmlFor="name">Email</label>
+              </div>
+            </div>
+            
+            <div className="inputSubBox">
+
+            <div className="input-container1">
+                <input id="name1" className="input1" type="password" pattern=".+" required />
+                <label className="label" htmlFor="name1">Password</label>
+              </div>
+            </div>
+
+            <div className="forgetDiv">
+              <div className="checkbox">
+                <input type="checkbox" className="check" />
+                <span>Remember Me</span>
+              </div>
+              <a href="#" className="a">Forget Password ?</a>
+            </div>
+
+            <button className="btn">LOGIN</button>
+
+            <p className="para">Or sign up using</p>
+
+            <div className="images">
+              <img src="https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png" alt="" />
+              <img src="https://www.pinclipart.com/picdir/middle/74-740310_transparent-background-twitter-logo-clipart.png" alt="" />
+            </div>
+
           </div>
-           <a href="#" className="a">Forget Password ?</a>
-          </div>
-
-           <button className="btn">LOGIN</button>
-
-          <p className="para">Or sign up using</p>
-
-           <div className="images">
-            <img src="https://1000logos.net/wp-content/uploads/2021/04/Facebook-logo.png" alt="" />
-            <img src="https://www.pinclipart.com/picdir/middle/74-740310_transparent-background-twitter-logo-clipart.png" alt="" />
-           </div>
-
-           </div>
-         </form>
-        </div>
+        </form>
+      </div>
     </div>
   )
 }
